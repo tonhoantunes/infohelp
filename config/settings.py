bc18e964f0e73910e43e4be616de6ba9370e4c42
Antônio Antunes <antunesantonio91@gmail.com>
2025-03-02 19:37:47 -0300

    adicionando host permitido para produção

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-y7+6smcfyy!hcw%mw_(2)_-h3b++&7@+mo@2)cq*k^(uj1c1nt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['infohelp.pythonanywhere.com']
 
 
 # Application definition
